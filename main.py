9291ed063d02c2eb02f04dc9bb3d2dd06c30ebc5
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-03 10:51:10 +0100

    ✨feat: create a graph definition

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+USERNAME = os.getenv("USERNAME")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 user_param = {
-    "token": "fdnmfoneof",
-    "username": "heyliceeee",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_param) # post request
+graph_endpoint = f"{pixela_endpoint}/{user_param['username']}/graphs"
+graph_param = {
+    "id": "daily-water",
+    "name": "Daily Hydration",
+    "unit": "L",
+    "type": "float",
+    "color": "ajisai"
+}
+headers = {"X-USER-TOKEN": TOKEN}
+
+response = requests.post(graph_endpoint, json=graph_param, headers=headers)
 print(response.text)
