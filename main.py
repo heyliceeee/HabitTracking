56c9b304430f8d7c5406ef227f47b3271bb3c882
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-06-03 11:31:39 +0100

    ✨feat: add a pixel to the habit tracker

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import os
 import requests
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 USERNAME = os.getenv("USERNAME")
 
+headers = {"X-USER-TOKEN": TOKEN}
 pixela_endpoint = "https://pixe.la/v1/users"
-user_param = {
+user_body = {
     "token": TOKEN,
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-graph_endpoint = f"{pixela_endpoint}/{user_param['username']}/graphs"
-graph_param = {
+graph_endpoint = f"{pixela_endpoint}/{user_body['username']}/graphs"
+graph_body = {
     "id": "daily-water",
     "name": "Daily Hydration",
     "unit": "L",
     "type": "float",
     "color": "ajisai"
 }
-headers = {"X-USER-TOKEN": TOKEN}
 
-response = requests.post(graph_endpoint, json=graph_param, headers=headers)
-print(response.text)
-# open url f"https://pixe.la/v1/users/{USERNAME}/graphs/daily-water"
+add_pixel_endpoint = f"{graph_endpoint}/daily-water"
+add_pixel_body = {
+    "date": datetime.datetime.now().strftime("%Y%m%d"),
+    "quantity": "1.5"
+}
+response = requests.post(url=add_pixel_endpoint, json=add_pixel_body, headers=headers)
